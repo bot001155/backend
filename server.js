4ba7ad07d55495b3252cde6715b60f8d55cb3969
bot001155 <jadukor473@gmail.com>
2026-01-24 11:30:19 +0530

    Update server.js

--- server.js
+++ server.js
@@ -377,9 +377,9 @@ app.post("/telegram-webhook", async (req, res) => {
    START SERVER
 ========================= */
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log("🚀 Server running on port", PORT);
-});
+app.listen(PORT, "0.0.0.0", () => console.log("Running on", PORT));
+;
+
 
 
 
